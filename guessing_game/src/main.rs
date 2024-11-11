670139d49048dece0c86ea37e2916ac9224f6201
Aditya Jha <aditya01jha.02@gmail.com>
2024-11-11 23:59:21 +0530

    Chapter 2 : Guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io; // standard library documentation - prelude
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess a Number !");
+    println!("Enter the Number :-");
+
+    let mut number = String::new(); // let is the declaration of variables 
+    // variables are by default of immutable type in rust and hence use mut to declare a mutable variable
+    // = is = ( equal to is equal to )
+    // String::new() -> string is a class and new is a function that assigns an empty string
+    io::stdin().read_line(&mut number).expect("You are inside expect ?? this is supposed to be error handling not the normal flow of the program");
+
+    println!("Number : {}", number);
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\adity\projects\rust\guessing_game\target\debug\guessing_game.exe: C:\Users\adity\projects\rust\guessing_game\src\main.rs
